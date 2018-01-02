4c0229e3ad38b1ecf3ea23f8139be5ae19c44c95
wangl <wanglswift@gmail.com>
2018-01-02 16:51:30 +0800

    init native socket project

---
+++ NativeSocket/NativeSocket/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NativeSocket
+//
+//  Created by wangliang on 2018/1/2.
+//  Copyright © 2018年 wangliang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
